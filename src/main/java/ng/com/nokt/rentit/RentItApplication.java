5553d8d51a41428c764dad37f613ad26385ea546
BarkaBoss <bossmajor.biii@gmail.com>
2024-03-31 22:41:36 +0100

    Added base components

---
+++ src/main/java/ng/com/nokt/rentit/RentItApplication.java
@@ -0,0 +1,13 @@
+package ng.com.nokt.rentit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentItApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ng/com/nokt/rentit/controller/HomeController.java
@@ -0,0 +1,13 @@
+package ng.com.nokt.rentit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "index";
+    }
+}
