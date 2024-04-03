9f408c22de2f9e92cbcea3b1fa59e2e7672bb329
BarkaBoss <bossmajor.biii@gmail.com>
2024-04-03 22:42:14 +0100

    Added Login interface

--- src/main/java/ng/com/nokt/rentit/controller/HomeController.java
+++ src/main/java/ng/com/nokt/rentit/controller/HomeController.java
@@ -1,11 +1,15 @@
 package ng.com.nokt.rentit.controller;
 
+import ng.com.nokt.rentit.entities.Account;
 import ng.com.nokt.rentit.entities.Post;
+import ng.com.nokt.rentit.services.AccountService;
 import ng.com.nokt.rentit.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -14,6 +18,8 @@ public class HomeController {
 
     @Autowired
     private PostService postService;
+    @Autowired
+    private AccountService accountService;
 
     @GetMapping("/")
     public String home(Model model){
@@ -23,7 +29,20 @@ public class HomeController {
     }
 
     @GetMapping("/register")
-    public String register(){
+    public String register(Model model){
+        Account account = new Account();
+        model.addAttribute("account", account);
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute Account account){
+        accountService.save(account);
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login(Model  model){
+        return "login";
     }
 }
--- src/main/java/ng/com/nokt/rentit/services/AccountService.java
+++ src/main/java/ng/com/nokt/rentit/services/AccountService.java
@@ -11,7 +11,7 @@ public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
-    public Account save(Account user){
-        return accountRepository.save(user);
+    public Account save(Account account){
+        return accountRepository.save(account);
     }
 }
