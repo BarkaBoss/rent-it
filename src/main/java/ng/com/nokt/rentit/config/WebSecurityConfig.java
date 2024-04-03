e5a5c375de0fc4200a0831a4a9b507734ab69ff2
BarkaBoss <bossmajor.biii@gmail.com>
2024-04-03 23:21:19 +0100

    Configured Spring Security

---
+++ src/main/java/ng/com/nokt/rentit/config/WebSecurityConfig.java
@@ -0,0 +1,68 @@
+package ng.com.nokt.rentit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+//@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebSecurityConfig {
+    private static final String[] WHITE_LIST = {
+            "/",
+            "/login",
+            "/register",
+            "/db-console/**",
+            "/static/**"
+    };
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .requestMatchers(WHITE_LIST)
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin(
+                        formLogin -> formLogin
+                                .loginPage("/login")
+                                .loginProcessingUrl("/login")
+                                .usernameParameter("email")
+                                .passwordParameter("password")
+                                .defaultSuccessUrl("/", true)
+                                .failureUrl("/login?error")
+                                .permitAll()
+                )
+                .logout(
+                        logout-> logout
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/logout?success")
+                ).httpBasic(Customizer.withDefaults());
+
+        http.csrf(
+                AbstractHttpConfigurer::disable
+        );
+        http.headers(
+                header -> header.frameOptions(
+                        HeadersConfigurer.FrameOptionsConfig::disable
+                ).disable()
+        );
+        return http.build();
+    }
+}
--- src/main/java/ng/com/nokt/rentit/services/AccountService.java
+++ src/main/java/ng/com/nokt/rentit/services/AccountService.java
@@ -3,6 +3,7 @@ package ng.com.nokt.rentit.services;
 import ng.com.nokt.rentit.entities.Account;
 import ng.com.nokt.rentit.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,10 @@ public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public Account save(Account account){
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accountRepository.save(account);
     }
 }
